6b3575b1a6248058127ccd2da30b1510320c8350
rshrc <rishieric91@gmail.com>
2018-11-18 12:12:59 +0530

    Added the Chat Room Application

---
+++ social_engine/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
+    path('chat/', include('chat.urls', namespace='chat')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
